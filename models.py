bc26d8bc1ce5537c0cae09583bc14860d87e6126
Haoran <haoran@bonsai.com>
2015-09-11 16:13:45 -0700

    init

---
+++ models.py
@@ -0,0 +1,9 @@
+import datetime
+from mongoengine import *
+from mongoengine.queryset import DoesNotExist
+
+connect("social")
+
+class Tweet(DynamicDocument):
+    id = LongField(primary_key=True)
+    pass
