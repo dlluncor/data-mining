8f96783a349b759ced4ff022136d31b85e4aeca6
dlluncor <david.lluncor@gmail.com>
2015-09-16 23:51:35 -0700

    A commit.

--- renters/renters_to_csv.py
+++ renters/renters_to_csv.py
@@ -3,8 +3,16 @@ import datetime
 from collections import OrderedDict
 from collections import namedtuple
 import renter_constants as c
+import itertools
+import random
 
-dobs = ['01/10/1990', '10/13/2005', '3/2/2000']
+def pick_from_list(l):
+  # Randomly pick from a list.
+  i = random.random()
+  return l[int(i * len(l))]
+
+
+dobs = ['01/10/1990', '10/13/2005', '3/2/2000'] # TODO
 property_worth = ['4000', '6000', '8000', '10000', '12000', '14000', '16000',
                   '18000', '20000', '22000', '24000', '26000', '28000', '30000',
                   '35000', '40000', '50000', '60000', '70000', '80000', '90000', '100000']
@@ -42,7 +50,7 @@ d = OrderedDict([
   ('Home Security?', (['Y', 'N'], 'iterate')),
   ('Non Smoking Household?', (['Y', 'N'], 'iterate')),
   ('Local Burglar Alarm?', (['Y', 'N'], 'iterate')),
-  ('Unusual hazards?', (['NONE'], 'iterate')),
+  ('Unusual hazards?', (['NONE'], 'fixed')),
   ('Dogs that bite?', (['Y', 'N'], 'iterate')),
   ('Run a business from home?', (['N'], 'fixed')),
   ('Start date', (['Keep default.'], 'fixed')),
@@ -56,22 +64,63 @@ d = OrderedDict([
   #('', ([], '')),
 ])
 
+import pdb
 
-
-def renter_lines(header):
+def renter_lines():
   # Keep information fixed and then do a search on many parameters.
   # Produce everything based on a fixed information.
+  iter_col_vals = []
+  iter_col_names = []
+  col_name_to_iter_index = {}
+  i = 0
   for k, v in d.iteritems():
-    k = k
     vc = Column._make(v)
-  return ''
+    if vc.select_type != 'iterate':
+      continue
+    # Find all combinations for these columns.
+    iter_col_names.append(k)
+    iter_col_vals.append(vc.values)
+    col_name_to_iter_index[k] = i
+    i += 1
+
+  print col_name_to_iter_index 
+
+  # Find all combinations of the columns to iterate through.
+  iter_col_rows = list(itertools.product(*iter_col_vals))
+  print len(iter_col_rows)
+  pdb.set_trace()
+
+  # Iterate through the columns finding the correct key and then 
+  # fill in the fixed or random columns.
+  csv_rows = []
+  for iter_row in iter_col_rows:
+    csv_row = []
+    for k, v in d.iteritems():
+      vc = Column._make(v)
+      if vc.select_type == 'iterate':
+        # Pick from the correct index of the iter row.
+        col_index = col_name_to_iter_index[k]
+        csv_row.append(iter_row[col_index])
+      elif vc.select_type == 'fixed':
+        # Choose the first element.
+        csv_row.append(vc.values[0])
+      elif vc.select_type == 'random':
+        # Random choose from the list of elements provided in the columns description.
+        csv_row.append(pick_from_list(vc.values))
+      else:
+        panic('Unrecognized type %s' % (vc.select_type))
+
+    csv_rows.append(','.join(csv_row))
+
+
+  return '\n'.join(csv_rows)
   
 
 def get_renters_csv():
   header = [k for k, v in d.iteritems()]
   lines = []
   lines.append(','.join(header))
-  lines += renter_lines(header)
+  lines += renter_lines()
   return '\n'.join(lines)
 
 
