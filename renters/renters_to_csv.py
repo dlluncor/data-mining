1d899e7f7e5eacd0cedc70eb11ecda1db3142e20
dlluncor <david.lluncor@gmail.com>
2015-09-17 13:44:39 -0700

    A commit.

--- renters/renters_to_csv.py
+++ renters/renters_to_csv.py
@@ -28,10 +28,11 @@ property_worth = ['4000', '8000', '12000', '16000',
                   '35000', '40000', '50000', '60000', '70000', '80000', '90000', '100000']
 loss_of_use = ['Keep default']
 medical_payments = ['1000', '2000']
-personal_liability = ['100,000', '300,000', '500,000']
+personal_liability = ['100000', '300000', '500000']
 deductible = ['100', '100 / 250', '250', '500', '750', '1000', '1500', '2500', '5000']
 
 Column = namedtuple('Column', 'values select_type')
+AddrInfo = namedtuple('AddrInfo', 'use_random shared_random_address_index')
 
 use_synonyms_cols = set(['Date of birth'])
 
@@ -74,9 +75,7 @@ d = OrderedDict([
   ('Medical payments', (medical_payments, 'iterate')),
   ('Personal liability', (personal_liability, 'iterate')),
   ('Farmers Identity Protection', (['N', 'Y'], 'fixed')), # Y / N
-  ('Deductible', (deductible, 'iterate')),
-  #('', ([], '')),
-  #('', ([], '')),
+  ('Deductible', (deductible, 'iterate'))
 ])
 
 import pdb
@@ -110,7 +109,27 @@ def renter_lines():
   iter_col_rows = list(itertools.product(*iter_col_vals))
   #pdb.set_trace()
 
-  def get_cell_value(k, vc, shared_random_address_index):
+  def is_address_column(k):
+    return k == 'Zip code' or k == 'City' or k == 'Address'
+
+  def get_address_value(k, address_info):
+    address_info.shared_random_address_index
+    cell_value = ''
+    addresses = c.rnd_addresses if address_info.use_random else c.addresses
+    zip_codes = c.rnd_zip_codes if address_info.use_random else c.zip_codes
+    cities = c.rnd_cities if address_info.use_random else c.cities
+
+    if k == 'Address':
+      cell_value = addresses[address_info.shared_random_address_index]
+    elif k == 'Zip code':
+      cell_value = zip_codes[address_info.shared_random_address_index]
+    elif k == 'City':
+      cell_value = cities[address_info.shared_random_address_index]
+    else:
+      raise Exception('Calling get_address_value with non-address column.')
+    return cell_value
+
+  def get_cell_value(k, vc, address_info):
     cell_value = ''
     if vc.select_type == 'iterate':
       # For debug purposes.
@@ -135,12 +154,8 @@ def renter_lines():
     if k in use_synonyms_cols:
       cell_value = pick_from_list(synonyms[cell_value])
     # Specifically for addresses we need to randomly group them all together but pick from the same synonym.
-    if k == 'Address':
-      cell_value = c.rnd_addresses[shared_random_address_index]
-    elif k == 'Zip code':
-      cell_value = c.rnd_zip_codes[shared_random_address_index]
-    elif k == 'City':
-      cell_value = c.rnd_cities[shared_random_address_index]
+    if is_address_column(k):
+      cell_value = get_address_value(k, address_info)
     return cell_value
 
   # Iterate through the columns finding the correct key and then 
@@ -151,10 +166,11 @@ def renter_lines():
     # For each row, some columns need to pick from the same index because they are all dependent. Lets
     # generate a shared_random_index with a fixed length. Specifically just for addresses.
     shared_random_address_index = random_index(c.rnd_addresses)
+    address_info = AddrInfo(use_random=False, shared_random_address_index=shared_random_address_index)
     for k, v in d.iteritems():
       # This loop chooses the correct value to pick for a particular column in a particular row.
       vc = Column._make(v)
-      cell_value = get_cell_value(k, vc, shared_random_address_index)
+      cell_value = get_cell_value(k, vc, address_info)
       csv_row.append(cell_value)
 
     csv_rows.append(','.join(csv_row))
@@ -162,8 +178,7 @@ def renter_lines():
   print len(csv_rows)
   if len(csv_rows) > 1000000:
     raise Exception('Too many permutations to rate!')
-  return '\n'.join(csv_rows)
-
+  
   # Fill in the values which were not generated in the cross product but we still want to explore the values for.
   # Use default values and fix everything else to the default value when using this particular type.
   cols_not_crossed = set([]) # E.g., {"Address": all_addresses}
@@ -171,23 +186,40 @@ def renter_lines():
     vc = Column._make(v)
     if vc.select_type == 'fixed' and len(vc.values) > 1:
       cols_not_crossed.add(k)
+
+  print 'Columns not crossed:\n%s' % (str(cols_not_crossed))
+
   # For all columns that are not crossed, now use all default values and vary just one parameter.
+  # We dont need to iterate through all values for Zip code and City since we they are tied to the unique address.
+  cols_not_crossed.remove('Zip code')
+  cols_not_crossed.remove('City')
+
+  extra_csv_rows = []
   for col_not_crossed in cols_not_crossed:
-    all_column_values = d[col_not_crossed]
+    column_obj = Column._make(d[col_not_crossed])
+    all_column_values = column_obj.values
+    #print 'Column: %s. Num values: %d' % (col_not_crossed, len(all_column_values))
     for i in xrange(1, len(all_column_values)):
       vary_value = all_column_values[i]
       csv_row = []
+      shared_random_address_index = random_index(c.addresses)
+      # If we are varying the Address, then we need to make sure this index is the same as the vary_value index.
+      if col_not_crossed == 'Address':
+        shared_random_address_index = i  # Make it equivalent to the index of the address we are varying.
+
+      address_info = AddrInfo(use_random=False, shared_random_address_index=shared_random_address_index)
       # Now construct the row but keep everything default expect for the column we are varying.
       for k, v in d.iteritems():
         if k == col_not_crossed:
           csv_row.append(vary_value)
         else:
-          csv_row.append(get_cell_value(k, vc, shared_random_address_index))
-      csv_rows.append(','.join(csv_row))
+          vc = Column._make(v)
+          csv_row.append(get_cell_value(k, vc, address_info))
+      extra_csv_rows.append(','.join(csv_row))
 
+  print len(extra_csv_rows)
+  csv_rows += extra_csv_rows
   print len(csv_rows)
-  if len(csv_rows) > 1000000:
-    raise Exception('Too many permutations to rate!')
   return '\n'.join(csv_rows)
   
 
