3d20aeda1c47c7f0fbcb5f7a6385e65034e37a0a
dlluncor <david.lluncor@gmail.com>
2015-09-16 22:55:17 -0700

    A commit.

---
+++ renters/renter_constants.py
@@ -0,0 +1,15 @@
+# All three lists need to be in the same order and need to contain every California zip code.
+addresses = ['766 Vaquero Drive', '3288 Bay Road']
+cities = ['Mountain View', 'Redwood City']
+zip_codes = ['94043', '94044']
+
+# Random
+# Top 50 common first names
+first_names = ['David', 'John', 'Mary']
+# Top 50 common last names
+last_names = ['Lluncor', 'Smith', 'OMalley']
+# 100 fake phone numbers with valid city code (first three numbers are valid).
+# http://www.zipcodestogo.com/California/
+phone_numbers = ['310-987-6896']
+# 100 fake email addresses.
+emails = ['random1@gmail.com', 'random2@gmail.com']
--- renters/renters_to_csv.py
+++ renters/renters_to_csv.py
@@ -3,22 +3,6 @@ import datetime
 from collections import OrderedDict
 from collections import namedtuple
 
-# All three lists need to be in the same order and need to contain every California zip code.
-addresses = ['766 Vaquero Drive', '3288 Bay Road']
-cities = ['Mountain View', 'Redwood City']
-zip_codes = ['94043', '94044']
-
-# Random
-# Top 50 common first names
-first_names = ['David', 'John', 'Mary']
-# Top 50 common last names
-last_names = ['Lluncor', 'Smith', 'OMalley']
-# 100 fake phone numbers with valid city code (first three numbers are valid).
-# http://www.zipcodestogo.com/California/
-phone_numbers = ['310-987-6896']
-# 100 fake email addresses.
-emails = ['random1@gmail.com', 'random2@gmail.com']
-
 hazards = []
 dobs = ['01/10/1990', '10/13/2005', '3/2/2000']
 
