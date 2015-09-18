502a052eb4fffea05f700ac0bb60b63805dfe0e6
dlluncor <david.lluncor@gmail.com>
2015-09-18 14:07:05 -0700

    Example SETI instance.

--- renters/gen_offline_model.py
+++ renters/gen_offline_model.py
@@ -1,17 +1,27 @@
 
 
 import logs_to_seti
+from ml import learner
+from ml import seti
 
-
-
-
+def create_seti(bfs, cfs):
+  s = seti.SETIExample()
+  for bf in bfs:
+    s.add_binary(bf[0], bf[1])
+  for cf in cfs:
+    s.add_continuous(cf[0], cf[1])
 
 def main():
   # Generate an offline model.
-  filenames = ['renters_8000.csv']
-  setis = logs_to_seti.generate_seti(filenames)
-  l = Learner(setis)
-  l.create_model()
+  filenames = ['data/tdg_v0.csv']
+  #setis = logs_to_seti.generate_seti(filenames)
+  setis = [create_seti([('gender', 'm')], [('age', 25)])]
+  l = learner.Learner()
+  l.reset()
+  l.learn(setis)
+  print 'Model: '
+  print l.create_model()
+  print 'Statistics: '
   print l.generate_statistics()
   # Write the model to a file.
 
--- renters/ml/learner.py
+++ renters/ml/learner.py
@@ -1,3 +1,20 @@
 """
   A learner takes SETI examples and generates a model from it.
 """
+
+class Learner(object):
+
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    pass
+
+  def learn(self, setis):
+    pass
+
+  def create_model(self):
+    pass
+
+  def generate_statistics(self):
+    pass
