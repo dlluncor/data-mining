653862c160ae6a7abb8a384730b03e0be3015fdd
Haoran <haoran@bonsai.com>
2015-09-18 17:19:44 -0700

    load and serve memorized price

--- renters/ml/serving_scorer.py
+++ renters/ml/serving_scorer.py
@@ -3,13 +3,25 @@
    or a linear regression model if we must estimate given unknown parameters.
 """
 
-import seti
+import csv, seti
 
 class SetiServer(object):
 
   def __init__(self):
-    pass
+    self.memorized_prices = self.load_memorized_prices('memorized-v0.csv')
 
+  def load_memorized_prices(self, filename):
+    """Load the model from a file which is really a pickled hashmap."""
+    # TODO(haoran): Read the memorized model file.
+    # Model consists of the learned model as well as the memorized model.
+    # E.g., memorized model is 'memorized-v0.pickle'.
+    prices = {}
+    with open(filename, 'rb') as fin:
+        reader = csv.reader(fin)
+        for key, price in reader:
+            prices[key] = price
+
+    return prices
   def load_model(self, model):
     """Load the model from a file which is really a pickled hashmap."""
     # TODO(haoran): Read the memorized model file.
@@ -19,7 +31,7 @@ class SetiServer(object):
 
   def score(self, seti_input):
     """Score the model based on the SETI data."""
-    
+
     # If we've seen this example before, return the exact price
     # found.
     # Find all feature indices in example.
@@ -28,7 +40,8 @@ class SetiServer(object):
     # score using model.
     features = seti.create_feature_vector(seti_input)
     key = seti.standard_repr(features)
+    if key in self.memorized_prices:
+        return self.memorized_prices[key]
     # TODO(haoran): Determine if the SETI example was already seen and if so
     # return the memorized value for that example.
     pass
-
