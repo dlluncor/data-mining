304b56d97f1eebdb9bf39aa24790cf71d9277f6d
Haoran <haoran@bonsai.com>
2015-09-19 12:44:32 -0700

    add more feature names

--- renters/logs_to_seti.py
+++ renters/logs_to_seti.py
@@ -8,7 +8,14 @@ _DEBUG = False
 
 def _to_renter_form(csv_line):
   #   Insurance Type,Zip code,First name,Last name,Date of birth,Gender,Address,City,State,Auto insurance coverage?,Property Type,# units,# unrelated roommates,# property losses in last 3 years,Phone number,Email address,Fire Sprinkler System?,Central Fire & Burglar Alarm?,Local Fire / Smoke Alarm?,Home Security?,Non Smoking Household?,Local Burglar Alarm?,Unusual hazards?,Dogs that bite?,Run a business from home?,Start date,Personal property worth,Loss of use,Medical payments,Personal liability,Farmers Identity Protection,Deductible,Policy number,Timestamp (seconds),Policy price,Name of agent,Address of agent
-  (insurance_type, zip_code, first_name, last_name, dob, gender, address, city, state, has_auto_insurance_coverage, property_type, unit_count, unrelated_roommates_count), rest, (policy_price, agent_name, agent_address) = csv_line[:13], csv_line[13:-3], csv_line[-3:]
+  (insurance_type, zip_code, first_name, last_name, dob, gender, address,
+  city, state, has_auto_insurance_coverage, property_type, unit_count,
+  unrelated_roommates_count, property_losses_count, phone_number, email,
+  has_fire_sprinkler_system, has_center_fire_burglar_alarm, has_local_fire_smoke_alarm,
+  has_home_security, is_non_smoking_household, has_local_burglar_alarm, has_unusual_hazards, has_bite_dog,
+  is_running_bussiness, start_date, personal_property_value, loss_of_use, medical_payment,
+  personal_liability, farmers_identity_protection, deductible, policy_number,
+  timestamp, policy_price, agent_name, agent_address) = csv_line
   info = {
     'insurance_type': insurance_type,
     'zip_code': zip_code,
