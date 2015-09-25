4f365109ef60ed6c1b0028d6a53819a907756ca5
Haoran <haoran@bonsai.com>
2015-09-25 15:06:21 -0700

    make service work

--- renters/service/app.py
+++ renters/service/app.py
@@ -1,9 +1,9 @@
 import datetime, logging, sys
+from flask import Flask, render_template, json, jsonify, request, send_from_directory
 import util
-from flask import render_template
+
 from logging import StreamHandler
 from flask import Flask, request, json, jsonify
-from models import Subscription, Feedback, Contact
 from config import config
 from errors import ChargeException
 from helper import *
@@ -24,18 +24,14 @@ ALREADY_SUBSCRIBED_MSG = "Thanks, you have already placed your deposit with this
 # Displaying templates.
 
 @app.route('/')
-def landing_page():
+def home_page():
     return util.render_common_template('index.html')
-
-# Sending assets.
-
-from flask import Flask, request, send_from_directory
 
 @app.route('/assets/<path:path>')
 def send_js(path):
     return send_from_directory('assets', path)
 
-@app.route('/tos')
+@app.route('/terms')
 def tos():
     return util.render_common_template('tos.html')
 
@@ -46,38 +42,6 @@ def privacy_policy():
 @app.route('/about')
 def about():
     return util.render_common_template('about.html')
-# Actual handlers
-
-@app.route('/subscribe', methods=['POST'])
-def subscribe():
-    email = request.form.get('email', None)
-
-    if not email:
-        return jsonify(status='fail', msg=MISS_EMAIL_MSG)
-
-    if not Subscription.is_exist(email):
-        sub = Subscription()
-        sub.email = email
-        sub.save()
-
-        return jsonify(status='success', msg=SUCCESS_MSG)
-
-    return jsonify(status='warn', msg=ALREADY_SUBSCRIBED_MSG)
-
-@app.route('/feedback', methods=['POST'])
-def feedback():
-    email = request.form.get('email', None)
-    comment = request.form.get('comment', None)
-
-    if not email:
-        return jsonify(status='fail', msg=MISS_EMAIL_MSG)
-
-    fb = Feedback()
-    fb.email = email
-    fb.comment = comment
-    fb.save()
-
-    return jsonify(status='success', msg=SUCCESS_MSG)
 
 @app.route('/error', methods=['GET'])
 def error():
@@ -91,48 +55,6 @@ def page_not_found(e):
 def handle_exception(error):
     return util.render_common_template('errors/500.html', err_msg=repr(error))
 
-@app.route('/phone_ins_form_completed', methods=['POST'])
-def phone_ins_form_completed():
-    q = request.form.get('q')
-    return jsonify(status='success', q=q)
-
-@app.route('/insurance-exchange', methods=['GET'])
-@crossdomain(origin='*')
-def insurance_exchange():
-    return jsonify(name='Loss/Theft Insurance', price='$7.99/month', description='Reduce you loss~enjoy your life.')
-
-@app.route('/contacts', methods=['POST'])
-@crossdomain(origin='*')
-def contacts():
-    email = request.form.get('email')
-    contact = Contact.get_by(email)
-    if contact:
-        return jsonify(status='warn', msg='You have already submited your address.')
-
-    try:
-        contact = Contact()
-
-        contact.email = email;
-
-        contact.first_name = request.form.get('firstName')
-        contact.last_name = request.form.get('lastName')
-
-        contact.address_line_1 = request.form.get('addressLine1')
-        contact.address_line_2 = request.form.get('addressLine2')
-
-        contact.city = request.form.get('city')
-        contact.state = request.form.get('state')
-        contact.zip_code = request.form.get('zipCode')
-        contact.phone_number = request.form.get('phoneNumber')
-
-        contact.save()
-
-    except Exception as e:
-        print("Ooops! got an error -> %s" % e)
-        return jsonify(status='fail', msg='Ooops, some error occurs. Please call us to get a shipping box.')
-
-    print("success for [%s]" % email)
-    return jsonify(status='success', msg='Thank you! We will send you a shipping box as soon as possible.')
 
 if __name__ == '__main__':
      app.run(debug=True, host='0.0.0.0', port=8080)
--- renters/service/models.py
+++ renters/service/models.py
@@ -1,82 +1,113 @@
-import datetime
+import datetime, re
 from mongoengine import *
 from mongoengine.queryset import DoesNotExist
 from config import config
 
 connect("mate", host=config.mongodb_uri)
 
-class Subscription(Document):
-    email = StringField(max_length=256, required=True)
-    created_at = DateTimeField(default=datetime.datetime.now)
+class RenterForm(DynamicDocument):
+    payment_info = ReferenceField('PaymentInfo')
 
-    meta = {
-        'allow_inheritance': True,
-        'indexes': ['email'],
-        'ordering': ['email']
-    }
+class PaymentInfo(Document):
+    renter_form = ReferenceField(RenterForm, reverse_delete_rule=CASCADE)
+    card_number = StringField(min_length=12, max_length=20, required=True)
+    card_expiration_date = StringField(min_length=5, max_length=5, required=True)
+    card_cvv = StringField(min_length=4, max_length=4, required=True)
+    status = StringField(max_length=128)
+
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
 
     @staticmethod
-    def is_exist(email):
-        try:
-            sub = Subscription.objects.get(email=email)
+    def luhn_validate(card_number):
+        """
+        Validate the credit card number
+
+        Params:
+            card_number: String[length=12-19]
+
+        Ref:
+            1. https://en.wikipedia.org/wiki/Bank_card_number
+            2. http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#JavaScript
+        """
+        # TODO skip validation of China UnionPay and Diners Club enRoute
+        if re.match(r'^(62|2014|2149)', card_number):
             return True
-        except DoesNotExist:
-            return False
 
-    @staticmethod
-    def get_by(email=None):
-        try:
-            sub = None
-            if email:
-                sub = Subscription.objects.get(email=email)
-            return sub
-        except DoesNotExist:
-            return None
+        r = [int(ch) for ch in card_number][::-1]
+        return (sum(r[0::2]) + sum(sum(divmod(d*2,10)) for d in r[1::2])) % 10 == 0
 
-class Feedback(Document):
-    email = StringField(max_length=256, required=True)
-    comment = StringField(max_length=1024)
+"""
+# INsurance Types
+INSURANCE_AUTO = 1
+INSURANCE_BUSINESS = 2
+INSURANCE_CONDO = 3
+INSURANCE_HOME = 4
+INSURANCE_LIFE = 5
+INSURANCE_RENTERS = 6
+
+# Gender
+GENDER_FEMALE = 1
+GENDER_MALE = 2
+
+
+PROPERTY_TYPE_RENTED_HOUSE_SINGLE_FAMILY = 1
+PROPERTY_TYPE_RENTED_APARTMENT_OR_CONDO  = 2
+PROPERTY_TYPE_RENTED_TOWNHOUSE           = 3
+PROPERTY_TYPE_RENTED_DUPLEX-OR_TRIPLEX   = 4
+PROPERTY_TYPE_RENTED_3M_HOME             = 5 # PROPERTY_TYPE_RENTED_MOBILE_OR_MANUFACTURED_OR_MODULAR_HOME
+PROPERTY_TYPE_AL_OR_NURSING_HOME         = 6 # PROPERTY_TYPE_ASSISTED_LIVING_OR_NURSING_HOME
+PROPERTY_TYPE_DORMITORY                  = 7
+PROPERTY_TYPE_OTHER                      = 8
+
+# Insurance Type,Zip code,First name,Last name,Date of birth,
+# Gender,Address,City,State,Auto insurance coverage?,Property Type,
+# units,# unrelated roommates,# property losses in last 3 years,Phone number,
+# Email address,Fire Sprinkler System?,Central Fire & Burglar Alarm?,
+# Local Fire / Smoke Alarm?,Home Security?,Non Smoking Household?,Local Burglar Alarm?,
+# Unusual hazards?,Dogs that bite?,Run a business from home?,Start date,Personal property worth,
+# Loss of use,Medical payments,Personal liability,Farmers Identity Protection,Deductible,
+# Policy number,Timestamp (seconds),Policy price,Name of agent,Address of agent,Elancer Name
+class BaseInsurance(Document):
+    insurnace_type = IntField(required=True)
+
     created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+
+class RenterInsurance(BaseInsurance):
+    property_type = IntField(required=True)
+    units_count   = IntField(required=True)
+    unrelated_roommates_count = IntField(required=True)
+    property_losses_count = IntField(required=True)
+
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+
+class House(Document):
+    zip_code = IntField(required=True)
+    address = StringField(max_length=256, required=True)
+    city = StringField(max_length=128, required=True)
+    state = StringField(max_length=128, required=True)
+
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+
+class User(Document):
+    first_name    = StringField(max_length=128, required=True)
+    last_name     = StringField(max_length=128, required=True)
+    date_of_birth = DateTimeField(default=datetime.datetime.now, required=True)
+    gender        = IntField(required=True)
+    email = StringField(max_length=256, required=True)
+
+
+    status = StringField(max_length=256)
+
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
 
     meta = {
         'allow_inheritance': True,
         'indexes': ['email'],
         'ordering': ['email']
     }
-
-class Contact(Document):
-    email = StringField(max_length=256, required=True)
-    first_name = StringField(max_length=256, required=True)
-    last_name = StringField(max_length=256, required=True)
-    address_line_1 = StringField(max_length=1024, required=True)
-    address_line_2 = StringField(max_length=1024)
-    city = StringField(max_length=256, required=True)
-    state = StringField(max_length=256, required=True)
-    zip_code = StringField(max_length=256, required=True)
-    phone_number = StringField(max_length=256, required=True)
-
-    created_at = DateTimeField(default=datetime.datetime.now)
-
-    meta = {
-        'allow_inheritance': True,
-        'indexes': ['email'],
-        'ordering': ['email']
-    }
-
-    @staticmethod
-    def is_exist(email):
-        try:
-            contact = Contact.objects.get(email=email)
-            return True
-        except DoesNotExist:
-            return False
-
-    @staticmethod
-    def get_by(email=None):
-        try:
-            contact = None
-            if email:
-                contact = Contact.objects.get(email=email)
-            return contact
-        except DoesNotExist:
-            return None
+"""
