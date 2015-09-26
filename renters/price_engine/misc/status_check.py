c7c78bfa6798fb25aa14dce939814d1c5bbc5e85
Haoran <haoran@bonsai.com>
2015-09-26 12:20:08 -0700

    add more function to status check script

--- renters/price_engine/misc/status_check.py
+++ renters/price_engine/misc/status_check.py
@@ -41,107 +41,123 @@ def execute_cmd(cmd):
     except subprocess.CalledProcessError as e:
         print e
 
-def check_machine_statues(machines,show_err_count=False):
-    for machine in machines:
-        is_missed = machine.get('missed', False)
-        machine_id = machine['id']
-        ip = machine['ip']
-        cmds = [
-            'ps aux | grep [r]uby',
-            'wc -l data-mining/renters/%s' %  ('data/missed_full_%s.csv' % machine_id if is_missed == True else 'full_crosses_renters_0921212303_%s.csv' % machine_id),
-            'tail data-mining/renters/data/out_full_%s_0921212303.log | grep HITTING' % machine_id,
-            'wc -l data-mining/renters/data/%s' % ('data/error_missed_full_%s.log' % machine_id if is_missed else 'error_full_0921212303_2_%s.log' % machine_id)
-        ]
-        if not show_err_count:
-            cmd = create_remote_cmd(ip, cmds[:3])
-            try:
-                output = subprocess.check_output(cmd, shell=True)
-                header = ">>>> [%s] %s" % (machine_id, ip)
-                if re.search('ruby browser_robot.rb', output):
-                    print(TextDecorator.sucess(header))
-                else:
-                    print(TextDecorator.fail(header))
+def check_status(machine,show_err_count=False):
+    is_missed = machine.get('missed', False)
+    machine_id = machine['id']
+    ip = machine['ip']
+    cmds = [
+        'ps aux | grep [r]uby',
+        'free | head -n 2',
+        'wc -l data-mining/renters/price_engine/data/%s' %  ('missed_full_3_%s.csv' % machine_id if is_missed == True else 'origin_data_set/full_crosses_renters_0921212303_%s.csv' % machine_id),
+        'tail data-mining/renters/price_engine/data/status_full_0921212303_3_%s.log | grep HITTING' % machine_id,
 
-                print output
-            except subprocess.CalledProcessError as e:
-                print e
+        'printf "last-success: "',
+        r'echo -n `tail -n 1 data-mining/renters/price_engine/data/success_full_0921212303_3_%s.log | cut -c7-9 | tr -d "," | tr -d "\""`' % machine_id,
+        'printf " count: "',
+        'wc -l data-mining/renters/price_engine/data/success_full_0921212303_3_%s.log | cut -d " " -f 1' % machine_id,
 
-        else:
-            process_cmd = create_remote_cmd(ip, [cmds[0]])
-            total_count_cmd = create_remote_cmd(ip, [cmds[1]])
-            err_count_cmd = create_remote_cmd(ip, [cmds[3]])
-            progress_cmd = create_remote_cmd(ip, [cmds[2]])
+        'printf "last-error: "',
+        r'echo -n `tail -n 1 data-mining/renters/price_engine/data/error_full_0921212303_3_%s.log | cut -c7-9 | tr -d "," | tr -d "\""`' % machine_id,
+        'printf " count: "',
+        'wc -l data-mining/renters/price_engine/data/error_full_0921212303_3_%s.log | cut -d " " -f 1' % machine_id,
 
-            try:
-                process_output = subprocess.check_output(process_cmd, shell=True)
-                header = ">>>> [%s] %s" % (machine_id, ip)
-                if re.search('ruby browser_robot.rb', process_output):
-                    print(TextDecorator.sucess(header))
-                else:
-                    print(TextDecorator.fail(header))
+        'wc -l data-mining/renters/price_engine/data/%s' % ('error_missed_full_3_%s.log' % machine_id if is_missed else 'error_full_0921212303_2_%s.log' % machine_id)
+    ]
+    if not show_err_count:
+        cmd = create_remote_cmd(ip, cmds[:-1])
+        try:
+            output = subprocess.check_output(cmd, shell=True)
+            header = ">>>> [%s] %s" % (machine_id, ip)
+            if re.search('ruby browser_robot.rb', output):
+                print(TextDecorator.sucess(header))
+            else:
+                print(TextDecorator.fail(header))
 
-                total_output = subprocess.check_output(total_count_cmd, shell=True)
-                m = re.search('^(\d+)', total_output)
-                if m:
-                    sys.stdout.write("total: %s " % m.group(1))
+            print output
+        except subprocess.CalledProcessError as e:
+            print e
 
-                err_output = subprocess.check_output(err_count_cmd, shell=True)
-                m = re.search('^(\d+)', err_output)
-                if m:
-                    print(TextDecorator.warning("err: %s" % m.group(1)))
-                else:
-                    print("")
+    else:
+        process_cmd = create_remote_cmd(ip, [cmds[0]])
+        total_count_cmd = create_remote_cmd(ip, [cmds[1]])
+        err_count_cmd = create_remote_cmd(ip, [cmds[3]])
+        progress_cmd = create_remote_cmd(ip, [cmds[2]])
 
-                progress_output = subprocess.check_output(progress_cmd, shell=True)
-                print(progress_output)
+        try:
+            process_output = subprocess.check_output(process_cmd, shell=True)
+            header = ">>>> [%s] %s" % (machine_id, ip)
+            if re.search('ruby browser_robot.rb', process_output):
+                print(TextDecorator.sucess(header))
+            else:
+                print(TextDecorator.fail(header))
 
-            except subprocess.CalledProcessError as e:
-                print e
+            total_output = subprocess.check_output(total_count_cmd, shell=True)
+            m = re.search('^(\d+)', total_output)
+            if m:
+                sys.stdout.write("total: %s " % m.group(1))
 
-def init_remote_env(machines, name, passwd):
-    for machine in machines:
-        ip = machine['ip']
-        machine_id = machine['id']
-        print("[%s] init env" % machine_id)
-        upload_file(ip, '~/locale', '~/locale')
-        upload_file(ip, '~/Xwrapper.config', '~/Xwrapper.config')
-        cmds = [
-            'rm -rf data-mining',
-            'git clone https://%s:%s@github.com/bonjoylabs/data-mining' % (name, passwd),
-            'sudo cp /etc/default/locale /etc/default/locale.default',
-            'sudo cp ~/locale /etc/default/locale',
-            'sudo cp /etc/X11/Xwrapper.config /etc/X11/Xwrapper.config.default',
-            'sudo cp ~/Xwrapper.config /etc/X11/Xwrapper.config'
-        ]
-        execute_remote_cmds(ip, cmds)
-        break
+            err_output = subprocess.check_output(err_count_cmd, shell=True)
+            m = re.search('^(\d+)', err_output)
+            if m:
+                print(TextDecorator.warning("err: %s" % m.group(1)))
+            else:
+                print("")
 
-def start_scripting(machines):
-    for machine in machines:
-        ip = machine['ip']
-        machine_id = machine['id']
-        print("[%s] start script" % machine_id)
-        print("Check if X server is running")
-        output = execute_remote_cmds(ip, ['pidof X'])
-        if output is None:
-            print("start X server")
-            output = execute_remote_cmds(ip, ['export DISPLAY=:0.0', 'nohup startx > /dev/null 2>&1 &'])
-        else:
-            print("X server is already running")
-        cmds = [
-            'cd ~/data-mining/renters/price_engine',
-            "nohup ruby browser_robot.rb 'data/origin_data_set/full_crosses_renters_0921212303_%s.csv' 'full_0921212303_3_%s' 0 >> data/out_full_3_%s.log > /dev/null 2>&1 &" % (machine_id, machine_id, machine_id)
-        ]
-        output = execute_remote_cmds(ip, cmds)
-        print(output)
-        break
+            progress_output = subprocess.check_output(progress_cmd, shell=True)
+            print(progress_output)
+
+        except subprocess.CalledProcessError as e:
+            print e
+
+def init_remote_env(machine, name, passwd):
+    ip = machine['ip']
+    machine_id = machine['id']
+    print("[%s] init env" % machine_id)
+    upload_file(ip, '~/locale', '~/locale')
+    upload_file(ip, '~/Xwrapper.config', '~/Xwrapper.config')
+    cmds = [
+        'rm -rf data-mining',
+        'git clone https://%s:%s@github.com/bonjoylabs/data-mining' % (name, passwd),
+        'sudo cp /etc/default/locale /etc/default/locale.default',
+        'sudo cp ~/locale /etc/default/locale',
+        'sudo cp /etc/X11/Xwrapper.config /etc/X11/Xwrapper.config.default',
+        'sudo cp ~/Xwrapper.config /etc/X11/Xwrapper.config'
+    ]
+    execute_remote_cmds(ip, cmds)
+
+def start_scripting(machine):
+    ip = machine['ip']
+    machine_id = machine['id']
+    print("[%s] start script" % machine_id)
+    print("Check if X server is running")
+    output = execute_remote_cmds(ip, ['pidof X'])
+    if output is None:
+        print("start X server")
+        output = execute_remote_cmds(ip, ['export DISPLAY=:0.0', 'nohup startx > /dev/null 2>&1 &'])
+    else:
+        print("X server is already running")
+    cmds = [
+        'export DISPLAY=:0.0',
+        'cd ~/data-mining/renters/price_engine',
+        "nohup ruby browser_robot.rb 'data/origin_data_set/full_crosses_renters_0921212303_%s.csv' 'full_0921212303_3_%s' 0 > /dev/null 2>&1 &" % (machine_id, machine_id),
+    ]
+    execute_remote_cmds(ip, cmds)
+
+def reboot_machine(machine):
+    ip = machine['ip']
+    execute_remote_cmds(ip, ['sudo reboot'])
+
+def restart_task(machine):
+    pass
+
+def resume_task(machine):
+    pass
 
 machines = [
-    {'id': 1,  'ip': '52.11.189.158'},# 'missed': True},
     {'id': 0,  'ip': '52.88.233.117'},# 'missed': True},
-
+    {'id': 1,  'ip': '52.11.189.158'},# 'missed': True},
     {'id': 2,  'ip': '52.88.226.131'},# 'missed': True},
-    {'id': 3,  'ip': '52.11.249.176'},# 'missed': True},
+    {'id': 3,  'ip': '52.27.101.36'},# 'missed': True},
     {'id': 4,  'ip': '52.89.19.195'} ,# 'missed': True},
     {'id': 5,  'ip': '52.88.52.137'} ,# 'missed': True},
     {'id': 6,  'ip': '52.24.245.42'} ,# 'missed': True},
@@ -152,11 +168,41 @@ machines = [
     {'id': 11, 'ip': '52.89.3.92'}   ,# 'missed': True},
     {'id': 12, 'ip': '52.88.149.7'}  ,#
     {'id': 13, 'ip': '52.88.119.125'},# 'missed': True},
-    {'id': 14, 'ip': '52.88.152.120'},# 'missed': True},
-    {'id': 15, 'ip': '52.27.163.93'}, # 'missed': True},
+    {'id': 14, 'ip': '52.24.188.108'},# 'missed': True},
+    {'id': 15, 'ip': '52.88.4.226'}, # 'missed': True},
 ]
 if __name__ == '__main__':
-    if sys.argv[1] == 'true':
-        #init_remote_env(machines, sys.argv[2], sys.argv[3])
-        start_scripting(machines)
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--id", help="id of the machine to operate. signle id or multiple ids delimit by comma.")
+    parser.add_argument("-a", "--action", help="[init | check | resume | reboot | restart] Action need to be executed. Signle action or multiple actions delimit by comma.")
+    args = parser.parse_args()
+    #if sys.argv[1] == 'true':
+    #    init_remote_env(machines, sys.argv[2], sys.argv[3])
+    #    start_scripting(machines)
+    #    pass
     #check_machine_statues(machines)
+    if args.id is None:
+        sys.exit("Sorry fail to execute. Please give the machine ids to operate")
+
+    if args.action is None:
+        sys.exit("Sorry fail to execute. Please give the actions to operate")
+
+    action_funcs = {
+        'init': init_remote_env,
+        'check': check_status,
+        'resume': resume_task,
+        'reboot': reboot_machine,
+        'restart': restart_task
+    }
+    if args.id == 'all':
+        target_machines = machines
+    else:
+        ids = args.id.split(',')
+        target_machines = [machine for machine in machines if str(machine['id']) in ids]
+
+    actions = args.action.split(',')
+    for machine in target_machines:
+        for name in actions:
+            action_funcs[name](machine)
