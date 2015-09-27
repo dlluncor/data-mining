407af54a64d3cb56803caf7801c1f10790dc2d6b
Haoran <haoran@bonsai.com>
2015-09-26 21:56:56 -0700

    make status check auto run missed data set

--- renters/price_engine/misc/status_check.py
+++ renters/price_engine/misc/status_check.py
@@ -19,7 +19,7 @@ class TextDecorator(object):
         return TextDecorator.FAIL + txt + TextDecorator.ENDC
 
     @staticmethod
-    def warning(txt):
+    def warn(txt):
         return TextDecorator.WARNING + txt + TextDecorator.ENDC
 
 def upload_file(ip, local_path, remote_path):
@@ -34,39 +34,78 @@ def execute_remote_cmds(ip, cmds):
     return execute_cmd(cmd)
 
 def execute_cmd(cmd):
+    print(cmd)
     try:
-        print(cmd)
         output = subprocess.check_output(cmd, shell=True)
         return output
     except subprocess.CalledProcessError as e:
         print e
 
-def get_last_success_id_cmd(machine_id):
-    return [
-        r'echo -n `tail -n 1 data-mining/renters/price_engine/data/success_full_0921212303_3_%s.log | cut -c7-9 | tr -d "," | tr -d "\""`' % machine_id,
-    ]
+def get_last_success_id_cmd(machine, use_origin=False):
+    cmd = r'FILE_PATH=data-mining/renters/price_engine/data/success_%s.log; test -e $FILE_PATH && echo -n `tail -n 1 $FILE_PATH | cut -c7-9 | tr -d "," | tr -d "\""`' % get_tag(machine)
+    if machine.get('handle_missed', False) and not use_origin:
+        cmd = r'FILE_PATH=data-mining/renters/price_engine/data/success_%s.log; test -e $FILE_PATH && echo -n `tail -n 1 $FILE_PATH | cut -c7-9 | tr -d "," | tr -d "\""`' % get_tag(machine)
+
+    return [cmd]
+
+def get_last_error_id_cmd(machine, use_origin=False):
+    cmd = r'FILE_PATH=data-mining/renters/price_engine/data/error_%s.log; test -e $FILE_PATH && echo -n `tail -n 1 $FILE_PATH | cut -c7-9 | tr -d "," | tr -d "\""`' % get_tag(machine)
+    if machine.get('handle_missed', False) and not use_origin:
+        cmd = r'FILE_PATH=data-mining/renters/price_engine/data/error_%s.log; test -e $FILE_PATH && echo -n `tail -n 1 $FILE_PATH | cut -c7-9 | tr -d "," | tr -d "\""`' % get_tag(machine)
+
+    return [cmd]
+
+def get_total_count_cmd(machine, use_origin=False):
+    cmd = r'wc -l data-mining/renters/price_engine/data/origin_data_set/full_crosses_renters_0921212303_%s.csv | cut -d " " -f 1' % machine['id']
+    if machine.get('handle_missed', False) and not use_origin:
+        cmd = r'wc -l data-mining/renters/price_engine/data/missed_full_%s.csv | cut -d " " -f 1' % machine['id']
+
+    return [cmd]
+
+def get_data_path(machine):
+    if machine.get('handle_missed', False):
+        return 'data/missed_full_%s.csv' % machine['id']
+    return 'data/origin_data_set/full_crosses_renters_0921212303_%s.csv' % machine['id']
+
+def get_tag(machine):
+    if machine.get('handle_missed', False):
+        return 'missed_full_3_%s' % machine['id']
+    return 'full_0921212303_3_%s' % machine['id']
+
+def generate_missed_data_set(machine):
+    print("generating missed data set")
+    ip = machine['ip']
+    machine_id = machine['id']
+    upload_file(ip, '~/extract_samples_from_error_file.py', '~/data-mining/renters/price_engine/misc/extract_samples_from_error_file.py')
+    execute_remote_cmds(ip, [
+        'cd data-mining/renters/price_engine',
+        'python misc/extract_samples_from_error_file.py %s %s' % ('data/error_full_0921212303_3_%s.log' % machine_id, 'data/missed_full_%s.csv' % machine_id)
+    ])
 
 def check_status(machine):
-    is_missed = machine.get('missed', False)
     machine_id = machine['id']
     ip = machine['ip']
+
+    if machine.get('finished', False):
+        print(TextDecorator.warn(">>>> [%s] %s FINISHED" % (machine_id, ip)))
+        return
+
     cmds = [
         'ps aux | grep [r]uby',
         'free | head -n 2',
-        'wc -l data-mining/renters/price_engine/data/%s' %  ('missed_full_3_%s.csv' % machine_id if is_missed == True else 'origin_data_set/full_crosses_renters_0921212303_%s.csv' % machine_id),
-        'tail data-mining/renters/price_engine/data/status_full_0921212303_3_%s.log | grep HITTING' % machine_id,
+    ] + get_total_count_cmd(machine) + [
+        'tail data-mining/renters/price_engine/data/status_%s.log | grep HITTING' % get_tag(machine),
 
         'printf "last-success: "',
-        ] + get_last_success_id_cmd(machine_id) + [
+    ] + get_last_success_id_cmd(machine) + [
         'printf " count: "',
-        'wc -l data-mining/renters/price_engine/data/success_full_0921212303_3_%s.log | cut -d " " -f 1' % machine_id,
+        'wc -l data-mining/renters/price_engine/data/success_%s.log | cut -d " " -f 1' % get_tag(machine),
 
         'printf "last-error: "',
-        r'echo -n `tail -n 1 data-mining/renters/price_engine/data/error_full_0921212303_3_%s.log | cut -c7-9 | tr -d "," | tr -d "\""`' % machine_id,
+    ] + get_last_error_id_cmd(machine) + [
         'printf " count: "',
-        'wc -l data-mining/renters/price_engine/data/error_full_0921212303_3_%s.log | cut -d " " -f 1' % machine_id,
+        'wc -l data-mining/renters/price_engine/data/error_%s.log | cut -d " " -f 1' % get_tag(machine),
     ]
-
     cmd = create_remote_cmd(ip, cmds)
     try:
         output = subprocess.check_output(cmd, shell=True)
@@ -81,8 +120,6 @@ def check_status(machine):
         print output
     except subprocess.CalledProcessError as e:
         print e
-
-
 
 def init_remote_env(machine, name, passwd):
     ip = machine['ip']
@@ -114,7 +151,7 @@ def start_scripting(machine, offset=0):
     cmds = [
         'export DISPLAY=:0.0',
         'cd ~/data-mining/renters/price_engine',
-        "nohup ruby browser_robot.rb 'data/origin_data_set/full_crosses_renters_0921212303_%s.csv' 'full_0921212303_3_%s' %s > /dev/null 2>&1 &" % (machine_id, machine_id, offset),
+        "nohup ruby browser_robot.rb '%s' '%s' %s > /dev/null 2>&1 &" % (get_data_path(machine), get_tag(machine), offset),
     ]
     execute_remote_cmds(ip, cmds)
 
@@ -127,34 +164,85 @@ def restart_task(machine):
 
 def resume_task(machine):
     print("resume [%s]" % machine['id'])
-    offset = execute_remote_cmds(machine['ip'], get_last_success_id_cmd(machine['id']))
-    print("get last success id ", offset)
+    update_machine_status([machine])
+
+    if machine.get('finished', False):
+        print("[] is fnished. Do not need to resume")
+        return
 
     reboot_machine(machine)
-
-    print("waiting for 60s")
+    print("rebooting ... waiting for 60s")
     time.sleep(60)
+
+    if machine.get('handle_missed', False):
+        print(">> Handle Missed Data Set")
+    else:
+        print(">> Handle Original Data Set")
+
+    offset = execute_remote_cmds(machine['ip'], get_last_success_id_cmd(machine)) or 0
+    print("get last success id ", offset)
+
+    if machine.get('handle_missed', False):
+        generate_missed_data_set(machine)
 
     start_scripting(machine, offset)
 
+def update_machine_status(machines):
+    print("update machine status ...")
+    for machine in machines:
+        if not machine.get('finished', False) and is_machine_finish_origin_data(machine):
+            machine['handle_missed'] = True
+
+        if not machine.get('finished', False) and is_machine_finish_missed_data(machine):
+            machine['finished'] = True
+
+def is_machine_finish_origin_data(machine):
+    ip = machine['ip']
+    machine_id = machine['id']
+
+    total_count = execute_remote_cmds(ip, get_total_count_cmd(machine, True)) or '0'
+    success_id = execute_remote_cmds(ip, get_last_success_id_cmd(machine, True)) or '0'
+    error_id = execute_remote_cmds(ip, get_last_error_id_cmd(machine, True)) or '0'
+
+    if int(success_id.strip()) + 1 == int(total_count.strip()) or int(error_id.strip()) + 1 == int(total_count.strip()):
+        return True
+    return False
+
+def is_machine_finish_missed_data(machine):
+    ip = machine['ip']
+    machine_id = machine['id']
+
+    if not machine.get('handle_missed', False):
+        return False
+
+    total_count = execute_remote_cmds(ip, get_total_count_cmd(machine)) or '0'
+    success_id = execute_remote_cmds(ip, get_last_success_id_cmd(machine)) or '0'
+    error_id = execute_remote_cmds(ip, get_last_error_id_cmd(machine)) or '0'
+
+    if int(success_id.strip()) + 1 == int(total_count.strip()) or int(error_id.strip()) + 1 == int(total_count.strip()):
+        return True
+    return False
+
 machines = [
-    {'id': 0,  'ip': '52.88.233.117'},# 'missed': True},
-    {'id': 1,  'ip': '52.11.189.158'},# 'missed': True},
-    {'id': 2,  'ip': '52.88.226.131'},# 'missed': True},
-    {'id': 3,  'ip': '52.27.101.36'},# 'missed': True},
-    {'id': 4,  'ip': '52.89.19.195'} ,# 'missed': True},
-    {'id': 5,  'ip': '52.88.52.137'} ,# 'missed': True},
-    {'id': 6,  'ip': '52.24.245.42'} ,# 'missed': True},
-    {'id': 7,  'ip': '52.25.250.151'},# 'missed': True},
-    {'id': 8,  'ip': '52.88.193.187'},# 'missed': True},
-    {'id': 9,  'ip': '52.88.8.172'}  ,# 'missed': True},
-    {'id': 10, 'ip': '52.88.252.120'},# 'missed': True},
-    {'id': 11, 'ip': '52.89.3.92'}   ,# 'missed': True},
-    {'id': 12, 'ip': '52.88.149.7'}  ,#
-    {'id': 13, 'ip': '52.88.119.125'},# 'missed': True},
-    {'id': 14, 'ip': '52.24.188.108'},# 'missed': True},
-    {'id': 15, 'ip': '52.88.4.226'}, # 'missed': True},
+    {'id': 0,  'ip': '52.88.233.117'},
+    {'id': 1,  'ip': '52.11.189.158'},
+    {'id': 2,  'ip': '52.88.226.131'},
+    {'id': 3,  'ip': '52.27.101.36'} ,
+    {'id': 4,  'ip': '52.89.19.195'} ,
+    {'id': 5,  'ip': '52.88.52.137'} ,
+    {'id': 6,  'ip': '52.24.245.42'} ,
+    {'id': 7,  'ip': '52.25.250.151'},
+    {'id': 8,  'ip': '52.88.193.187'},
+    {'id': 9,  'ip': '52.88.8.172'}  ,
+    {'id': 10, 'ip': '52.88.252.120'},
+    {'id': 11, 'ip': '52.89.3.92'}   ,
+    {'id': 12, 'ip': '52.88.149.7'}  ,
+    #{'id': 13, 'ip': '52.88.119.125'},
+    {'id': 14, 'ip': '52.24.188.108'},
+    #{'id': 15, 'ip': '52.88.4.226'}  ,
 ]
+
+
 if __name__ == '__main__':
     import argparse
 
@@ -163,11 +251,7 @@ if __name__ == '__main__':
     parser.add_argument("-a", "--action", help="[init | check | resume | reboot | restart] Action need to be executed. Signle action or multiple actions delimit by comma.")
     parser.add_argument("-l", "--loop", help="[forever | N] repeat the action for specific time")
     args = parser.parse_args()
-    #if sys.argv[1] == 'true':
-    #    init_remote_env(machines, sys.argv[2], sys.argv[3])
-    #    start_scripting(machines)
-    #    pass
-    #check_machine_statues(machines)
+
     if args.id is None:
         sys.exit("Sorry fail to execute. Please give the machine ids to operate")
 
@@ -179,14 +263,17 @@ if __name__ == '__main__':
         'check': check_status,
         'resume': resume_task,
         'reboot': reboot_machine,
-        'restart': restart_task
+        'restart': restart_task,
+        'test': is_machine_finish_origin_data
     }
+
     if args.id == 'all':
         target_machines = machines
     else:
         ids = args.id.split(',')
         target_machines = [machine for machine in machines if str(machine['id']) in ids]
 
+    update_machine_status(target_machines)
     actions = args.action.split(',')
 
     for machine in target_machines:
@@ -199,4 +286,6 @@ if __name__ == '__main__':
             for machine in target_machines:
                 for name in actions:
                     action_funcs[name](machine)
+            # run check every 5min
+            print("waiting for 5m ...")
             time.sleep(300)
