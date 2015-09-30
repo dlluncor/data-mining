5e66b8ef8eb4816fa80c3b78c844ffe71ce915f1
Haoran <haoran@bonsai.com>
2015-09-29 22:02:03 -0700

    update tests and dependencies

--- renters/payment_service/requests.sh
+++ renters/payment_service/requests.sh
@@ -1,43 +1,15 @@
 set -e
 #set -x
 
-BASE_URL=http://localhost:8080
+BASE_URL=http://localhost:8888
 #BASE_URL=http://immense-badlands-3000.herokuapp.com
 
-# Test when the user wants to know the price of the insurance.
-curl -H "Content-Type: application/json" -X POST -d  '
-{
-    "renter_form": {
-       "insurance_type": "Renters",
-       "first_name": "Christian",
-       "last_name": "Bale",
-       "dob": "01/30/1974",
-       "gender": "m",
-       "address": "3328 Bay Road",
-       "city": "Rewood City",
-       "state": "CA",
-       "zip_code": "94063",
-       "purchase_category": "cheap"
-    }
-}' \
-$BASE_URL/price
-
-printf '\n-----------------------------------\n'
-
-# Test when the user purchases insurance.
-curl -H "Content-Type: application/json" -X POST -d  '
-{
-    "renter_form": {
-       "insurance_type": "Renters",
-       "first_name": "Christian",
-       "last_name": "Bale",
-       "dob": "01/30/1974",
-       "gender": "m",
-       "address": "3328 Bay Road",
-       "city": "Rewood City",
-       "state": "CA",
-       "zip_code": "94063",
-       "purchase_category": "medium"
-    }
-}' \
-$BASE_URL/buy
+# Test valid card
+echo 'Test Valid Card'
+curl -X POST -d "number=4111111111111111&expiration_date=10/24&cvv=1234" $BASE_URL/credit_cards
+# Test Card with invalid number
+curl -X POST -d "number=4111111111111112&expiration_date=10/24&cvv=1234" $BASE_URL/credit_cards
+# Test Expired Card
+curl -X POST -d "number=4111111111111111&expiration_date=10/14&cvv=1234" $BASE_URL/credit_cards
+# Test Card with invalid cvv
+curl -X POST -d "number=4111111111111111&expiration_date=10/14&cvv=123" $BASE_URL/credit_cards
