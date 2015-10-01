8540223808112d8d7aef62e6cf1f859d9583bbc5
Haoran <haoran@bonsai.com>
2015-10-01 16:00:35 -0700

    minor changes

--- renters/service/deploy.py
+++ renters/service/deploy.py
@@ -1,4 +1,5 @@
-import subprocess, os, re, sys, time
+import logging, os, re, subprocess, sys, time
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 class TextDecorator(object):
     HEADER = '\033[95m'
@@ -11,7 +12,7 @@ class TextDecorator(object):
     UNDERLINE = '\033[4m'
 
     @staticmethod
-    def sucess(txt):
+    def success(txt):
         return TextDecorator.OKGREEN + txt + TextDecorator.ENDC
 
     @staticmethod
@@ -27,14 +28,13 @@ def upload_file(ip, local_path, remote_path):
     execute_cmd(cmd)
 
 def create_remote_cmd(ip, cmds):
-    return "ssh -X -i ~/.ssh/bonjoy-team.pem ubuntu@%s '%s'" % (ip, ';'.join(cmds))
+    return "ssh -i ~/.ssh/bonjoy-team.pem ubuntu@%s '%s'" % (ip, ';'.join(cmds))
 
 def execute_remote_cmds(ip, cmds):
     cmd = create_remote_cmd(ip, cmds)
     return execute_cmd(cmd)
 
 def execute_cmd(cmd):
-    print(cmd)
     try:
         output = subprocess.check_output(cmd, shell=True)
         return output
@@ -51,36 +51,40 @@ def restart_service(machine):
     execute_remote_cmds(ip, ['sudo restart renters'])
 
 def deploy(machine, name, passwd):
+    logging.info(TextDecorator.success("Deploying ..."))
     ip = machine['ip']
-    if name is None or passwd is None:
-        print("Please provide name and password. quit.")
-        print("Usage: NAME=xxx PASSWD=xxx python delpoy -i all -a deploy")
-        return
+
     cmds = [
         'cd /u/app/data-mining',
+        'printf ">>>> Update Codes\n"',
         'git pull --rebase https://%s:%s@github.com/bonjoylabs/data-mining master' % (name, passwd),
-        'sudo restart renters'
+        'git log -1',
+        'printf "\n>>>> Restart service\n"',
+        'sudo restart renters',
     ]
-    execute_remote_cmds(ip, cmds)
+    out = execute_remote_cmds(ip, cmds)
+    logging.info(out)
+    logging.info(TextDecorator.success("Deployment is finished."))
 
 def fresh_deploy(machine, name, passwd):
+    logging.info(TextDecorator.success("Fresh Deploying ..."))
     ip = machine['ip']
-    if name is None or passwd is None:
-        print("Please provide name and password. quit.")
-        print("Usage: NAME=xxx PASSWD=xxx python fresh_deploy -i all -a deploy")
-        return
 
     cmds = [
+        'sudo stop renters'
         'rm -rf /u/app/data-mining',
         'cd /u/app/',
         'git clone https://%s:%s@github.com/bonjoylabs/data-mining' % (name, passwd),
-        'sudo restart renters'
+        'cd /u/app/data-mining/renters/service',
+        'sudo pip install -r requirements.txt'
+        'sudo start renters'
     ]
     execute_remote_cmds(ip, cmds)
+    logging.info(TextDecorator.success("Deployment is finished."))
 
-def show_errors(machine, lines=20):
+def show_logs(machine, lines=20):
     ip = machine['ip']
-    execute_remote_cmds(ip, ['sudo tail -n %s /var/log/renters.log' % lines])
+    execute_remote_cmds(ip, ['sudo tail -n %s /var/log/upstart/renters.log' % lines])
 
 machines = [
     {'id': 0,  'ip': '52.89.174.57'},
@@ -91,16 +95,11 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--id", help="id of the machine to operate. signle id or multiple ids delimit by comma or all for all servers.")
-    parser.add_argument("-a", "--action", help="[deploy | fresh_deploy | reboot | restart | show_errors] Action need to be executed")
+    parser.add_argument("-a", "--action", help="[deploy | fresh_deploy | reboot | restart | logs] Action need to be executed")
     args = parser.parse_args()
 
-    if args.id is None:
-        print("Sorry fail to execute. Please give the machine ids to operate")
-        sys.exit(parser.print_help())
-
     if args.action is None:
-        print("Sorry fail to execute. Please give the actions to operate")
+        print("Sorry fail to execute. Please give the action to operate")
         sys.exit(parser.print_help())
 
     action_funcs = {
@@ -108,20 +107,15 @@ if __name__ == '__main__':
         'fresh_deploy': fresh_deploy,
         'reboot': reboot_machine,
         'restart': restart_service,
-        'show_errors': show_errors,
+        'logs': show_logs,
     }
 
-    if args.id == 'all':
-        target_machines = machines
-    else:
-        ids = args.id.split(',')
-        target_machines = [machine for machine in machines if str(machine['id']) in ids]
-
-    for machine in target_machines:
+    for machine in machines:
         if args.action == 'deploy' or args.action == 'fresh_deploy':
-            name = os.environ.get('NAME')
-            passwd = os.environ.get('PASSWD')
-            print(name, passwd)
-            action_funcs[args.action](machine, name, passwd)
+            import getpass
+
+            user = raw_input('Please input your github account.\nUsername: ')
+            passwd = getpass.getpass()
+            action_funcs[args.action](machine, user, passwd)
         else:
             action_funcs[args.action](machine)
